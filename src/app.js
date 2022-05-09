060b221a8034c09733691aeb7f4866daac9e13e2
kaiserone01 <mrporteles@gmail.com>
2022-05-09 12:42:19 -0400

    random card

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
+/* import "bootstrap"; */
+/* import "./style.css"; */
+/* ♦ ♥ ♣ ♠ */
+/* 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+ */
+let carta = {
+  simboloarriba: ["♦", "♥", "♣", "♠"], simboloabajo: ["♦", "♥", "♣", "♠"], numer: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-window.onload = function() {
-  //write your code here
+}
+
+function generacartas() {
+  let cardnumber = Math.floor(Math.random() * carta.numer.length)
+  document.querySelector("#randonnumber").innerHTML = carta.numer[cardnumber]
+
+  {
+    let cardnumber = Math.floor(Math.random() * carta.simboloarriba.length)
+  document.querySelector("#iconoizquierdo").innerHTML = carta.simboloarriba[cardnumber]
+  }
+  {
+    let cardnumber = Math.floor(Math.random() * carta.simboloabajo.length)
+  document.querySelector("#iconoderecho").innerHTML = carta.simboloabajo[cardnumber]
+  }
+
+  
+}
+
+window.onload = function () {
+
+  generacartas();
+
+
   console.log("Hello Rigo from the console!");
 };
