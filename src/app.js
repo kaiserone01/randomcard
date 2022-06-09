b915a7a1935c9adb5c122a315547b6e519ab3893
kaiserone01 <56692101+kaiserone01@users.noreply.github.com>
2022-06-09 16:39:14 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,36 +1,45 @@
 /* eslint-disable */
-/* import "bootstrap"; */
-/* import "./style.css"; */
-/* ♦ ♥ ♣ ♠ */
-/* 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
- */
-let carta = {
-  simboloarriba: ["♦", "♥", "♣", "♠"], simboloabajo: ["♦", "♥", "♣", "♠"], numer: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+import "bootstrap";
+import "./style.css";
 
-}
+window.onload = () => {
+  document.querySelector(".number").innerHTML = generadorDeNumeros();
+  let suit = generadorDeSuits();
+  document.querySelector(".topSuit").innerHTML = suit;
+  document.querySelector(".bottomSuit").innerHTML = suit;
+  document.querySelector(".card").classList.add(generadorDeClases(suit));
+};
 
-function generacartas() {
-  let cardnumber = Math.floor(Math.random() * carta.numer.length)
-  document.querySelector("#randonnumber").innerHTML = carta.numer[cardnumber]
+let generadorDeNumeros = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indiceN = ~~(Math.random() * numbers.length);
+  return numbers[indiceN];
+};
 
-  {
-    let cardnumber = Math.floor(Math.random() * carta.simboloarriba.length)
-  document.querySelector("#iconoizquierdo").innerHTML = carta.simboloarriba[cardnumber]
+let generadorDeSuits = () => {
+  let suits = ["♠", "♣", "♥", "♦"];
+  let indiceS = ~~(Math.random() * suits.length);
+  return suits[indiceS];
+};
+
+let generadorDeClases = suit => {
+  if (suit === "♥" || suit === "♦") {
+    document.querySelector(".card").classList.add("red");
+  } else {
+    document.querySelector(".card").classList.add("black");
   }
-  {
-    let cardnumber = Math.floor(Math.random() * carta.simboloabajo.length)
-  document.querySelector("#iconoderecho").innerHTML = carta.simboloabajo[cardnumber]
-  }
-
-  
-}
-
-window.onload = function () {
-
-  generacartas();
-
-
-  console.log("Hello Rigo from the console!");
 };
